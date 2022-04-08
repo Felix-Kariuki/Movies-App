9619edd7681e4f59d534ebdf61cbc2456f5ffc78
Felix-Kariuki <felixmurimi280@gmail.com>
2022-04-08 20:01:59 +0300

    screenshots

--- app/src/main/java/com/flexcode/movie/util/Constants.kt
+++ app/src/main/java/com/flexcode/movie/util/Constants.kt
@@ -5,7 +5,6 @@ object Constants {
     const val API_KEY = "0e6a03287bf29e1b8c64d02e9997b9e4"
 
     const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
-
     const val YOUTUBE_URL = "https://www.youtube.com/watch?v="
 
 }
