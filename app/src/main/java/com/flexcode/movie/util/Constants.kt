5d58673d79268bb0c70b47aa86a18f6c69d13df4
Felix Kariuki <61313608+Felix-Kariuki@users.noreply.github.com>
2022-04-09 08:44:47 +0300

    Update Constants.kt

--- app/src/main/java/com/flexcode/movie/util/Constants.kt
+++ app/src/main/java/com/flexcode/movie/util/Constants.kt
@@ -2,7 +2,7 @@ package com.flexcode.movie.util
 
 object Constants {
     const val BASE_URL ="https://api.themoviedb.org/3/"
-    const val API_KEY = "0e6a03287bf29e1b8c64d02e9997b9e4"
+    const val API_KEY = ""
 
     const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
     const val YOUTUBE_URL = "https://www.youtube.com/watch?v="
